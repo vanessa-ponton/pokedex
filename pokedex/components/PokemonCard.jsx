0ece6f57946ea17e42d1f5b7e89cec22a044c3ff
Vanessa Ponton <vanessa.ponton@gmail.com>
2023-11-24 14:59:15 +0100

    code

---
+++ pokedex/components/PokemonCard.jsx
@@ -0,0 +1,14 @@
+
+
+import PokemonCard from "./components/PokemonCard"
+function () {
+  return (
+    <figure>
+      <classImg src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png'></classImg>
+      <figcaption >Bulbasaur</figcaption>
+    </figure>
+  );
+}
+
+import PokemonCard from "./components/PokemonCard"
+export default ; <i></i>
