6914febbac556d066cb3cfdd14fc7bf6a93dd90e
Vanessa Ponton <vanessa.ponton@gmail.com>
2023-11-27 10:14:40 +0100

    react

--- pokedex/src/App.jsx
+++ pokedex/src/App.jsx
@@ -1,1 +1,20 @@
-<div>PokemonCard</div>
+
+import PokemonCard from "./components/PokemonCard";
+
+
+import './index.css';
+
+function App() {
+    return (
+        <div>
+            <PokemonCard />
+        </div>
+
+    );
+}
+
+export default App;
+
+
+
+
