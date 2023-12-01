e72efa6d73e731df2dd5c21812346334d39bba45
Vanessa Ponton <vanessa.ponton@gmail.com>
2023-12-01 11:31:15 +0100

    props

--- pokedex/src/App.jsx
+++ pokedex/src/App.jsx
@@ -4,23 +4,24 @@ import PokemonCard from "./components/PokemonCard";
 
 import './index.css';
 
-const pokemonList = [
-    {
-        name: "bulbasaur",
-        imgSrc:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-        name: "mew",
-    },
-];
 
-function App(pokemon) {
-    pokemon = pokemonList[0];
-    console.log(pokemon);
+
+function App() {
+    const pokemonList = [
+        {
+            name: "bulbasaur",
+            imgSrc:
+                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+        },
+        {
+            name: "mew",
+        },
+    ];
+    const pokemonSelected = pokemonList[0];
+
     return (
         <div>
-            <PokemonCard pokemon={pokemonList[0]} />
+            <PokemonCard pokemon={pokemonSelected} />
 
         </div>
     );
