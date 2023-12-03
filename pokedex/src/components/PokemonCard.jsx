742e7d8a6dafa21011cb3ad17451e8c159d8ef90
Vanessa Ponton <vanessa.ponton@gmail.com>
2023-12-03 07:35:29 +0100

    addpropTypes

--- pokedex/src/components/PokemonCard.jsx
+++ pokedex/src/components/PokemonCard.jsx
@@ -1,11 +1,19 @@
+import PropTypes from "prop-types";
+
 function PokemonCard(props) {
   const { pokemon } = props;
   console.log(props);
 
+  PokemonCard.propTypes = {
+    pokemon: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      imgSrc: PropTypes.string.isOptionnal,
+    }).isRequired,
+  }
 
   return (
     <figure>
-      {pokemon.imgSrc ? (<img src={pokemon.imgSrc} />) : (<p>???</p>)};
+      {pokemon.imgSrc ? (<img src={pokemon.imgSrc} />) : (<p>???</p>)}
       <figcaption >{pokemon.name}</figcaption>
     </figure>
 
